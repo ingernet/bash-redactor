65f00a4a36e399e15f1163b1fe7cbbe437b6cab8
inger klekacz <ingernet@users.noreply.github.com>
2018-03-07 15:33:01 -0800

    tidying up mkdsamples (#3)

--- mksamples.bash
+++ mksamples.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 # I generate some files to work with based off of sample.log
 
+mkdir -p ziplogs;
+
 counter=1
 while [ $counter -le 10 ]; do
     cp sample.log ziplogs/${counter}.log &&
@@ -8,4 +10,5 @@ while [ $counter -le 10 ]; do
     ((counter++))
 done
 
-echo "sample files generated in ziplogs/";
+echo "sample files generated in ziplogs/" 1>&2;
+echo "use them by running: `sudo ./redactor.bash -d ziplogs`" 1>&2;
