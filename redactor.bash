714ab01bce78a2edd01e4596ae3e2c60f48deac3
Inger Klekacz <ingernet@gmail.com>
2018-03-07 11:46:44 -0800

    filling out the -f argument

--- redactor.bash
+++ redactor.bash
@@ -19,85 +19,74 @@
 # 8. preserve as much metadata in redacted copies as possible
 
 ## SETUP
+# To add an additional field to redact, add it to the array below.
+scrubbed_fields=('CC' 'SSN')
 
 ## USAGE
 usage() { 
     echo "Usage: " 1>&2;
-    echo "Specify a few source files with: $0 -f file1 [file2 file3 etc.]" 1>&2;
+    echo "Specify a few source files with: $0 -f file1.log.gz [file2.log.gz file3.log.gz etc.]" 1>&2;
     echo "Specify an entire source directory with: $0 -d directory_name" 1>&2;
-    echo ""; exit 0;
+    echo "";
+}
+
+create_working_dir() {
+    # create a subdir of /tmp
+    working_dir="/tmp/redactr_$(date "+%Y%m%d%H%M%S")";
+    mkdir -p ${working_dir} &&
+    echo "working directory created: ${working_dir}";
 }
 
 redact_file() {
-    $f = ${1}
+    f=${1};
     cp "$f" "$f~" &&
-    gzip -cd "$f~" | sed '/CC="[^"]*"/ s//CC="REDACTED"/g' | sed '/SSN="[^"]*"/ s//SSN="REDACTED"/g' | gzip >"$f"
+    gzip -cd "$f~" | sed '/ CC="[^"]*"/ s//CC="REDACTED"/g' | sed '/ SSN="[^"]*"/ s//SSN="REDACTED"/g' | gzip >"$f"
     rm "$f~";
 }
 
 redact_directory() {
-    # create a subdir of /tmp
-    working_dir="/tmp/redactr_$(date "+%Y%m%d%H%M%S")";
-    mkdir -p ${working_dir};
-    echo "working directory created: ${working_dir}" &&
 
     # make a copy of your originals
     echo "creating copy of files from ${1}"
-    sudo cp -p ${1}/*.gz ${working_dir}/
+    cp -p ${1}/*.gz ${working_dir}/
 
     echo "scrubbing copied files";
     for f in ${working_dir}/*; do
-        redact_file $
-        # cp "$f" "$f~" &&
-        # gzip -cd "$f~" | sed '/CC="[^"]*"/ s//CC="REDACTED"/g' | sed '/SSN="[^"]*"/ s//SSN="REDACTED"/g' | gzip >"$f"
-        # rm "$f~";
+        redact_file $f;
     done
 
     echo "done. go check out your files in: ${working_dir}";
 }
 
 # accept either a 1+ list of files with -f, or a directory, with -d
-while getopts "fd:" OPTION
+while getopts ":f:d:" OPTION
 do
     case $OPTION in
         f)
-            echo "You set flag -f"
-            # [[ -n "$gce_name" ]] || usage
+            srcfiles=${OPTARG};
+            echo "srcfiles: ${srcfiles}";
+            [[ -n ${srcfiles} ]] || usage;
+            echo "running redactor on: ${srcfiles}"
+            create_working_dir &&
+            for x in ${srcfiles}; do
+                cp -p ${x} ${working_dir}
+            done
+            
+            for x in ${working_dir}/*; do 
+                redact_file ${x};
+            done
+            # exit;
             ;;
         d)
             srcdir=${OPTARG}
             [[ -n "$srcdir" ]] || usage;
+            create_working_dir &&
             redact_directory ${srcdir};
-            exit;
+            # exit;
             ;;
         \?)
-            usage
+            usage;
+            exit 1;
             ;;
     esac
 done
-
-# To add an additional field to redact, add it to the array below.
-redacted_fields=('CC' 'SSN')
-
-
-# Loop through list of redacted_fields and do a search and replace for each field's contents.
-do_redact() {
-    echo "Redacting $1..." 1>&2
-    for i in "${redacted_fields[@]}"
-    do
-	    grep $i ${1}
-    done
-}
-
-duplicate_file() {
-    cp -p ${1} ${working_dir}
-}
-
-duplicate_dir() {
-    cp -pi ${1} ${working_dir}
-}
-
-
-
-
-# redact_files;
